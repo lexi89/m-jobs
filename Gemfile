0438281042a953ae5b98ad59fa536274fac7b200
Alexey <alexey.yanovski1@gmail.com>
2016-02-21 12:15:31 +0800

    added image upload

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem "mini_magick"
 gem 'carrierwave'
 gem 'bourbon'
 gem 'bootstrap-sass', '~> 3.3.6'
--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -69,6 +69,6 @@ class CompaniesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def company_params
-      params.require(:company).permit(:mission, :description, :name, :industry_ids)
+      params.require(:company).permit(:mission, :description, :name, :industry_ids, :logo)
     end
 end
